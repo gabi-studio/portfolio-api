8abe3a67006f95cbd95c00f445da03e1e91a3234
Gabi Catalan <gabistudio.dev@gmail.com>
2025-07-13 17:29:42 -0400

    Added sort order field to projects

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -2,7 +2,9 @@ const ProjectItem = require('../models/projectItem');
 
 exports.getAllProjects = async (req, res) => {
     try {
-        const projects = await ProjectItem.find().populate('techItems');
+        const projects = await ProjectItem.find()
+        .sort({ order: 1 })
+        .populate('techItems');
         res.json(projects);
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch projects' });
